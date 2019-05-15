3aba171c97a1df36011bae96078d63911a6e7342
mihailtudos <mihailtudos@outlook.com>
2019-05-15 11:28:29 +0100

    View all bookings done

--- src/ConfirmPayment.java
+++ src/ConfirmPayment.java
@@ -20,7 +20,7 @@ public class ConfirmPayment extends javax.swing.JFrame {
     String userName;
     Connection connection;
     ResultSet result;
-    PreparedStatement getUserBooking;
+    PreparedStatement confirmPayment, getUserBooking;
     
     
    
@@ -48,6 +48,7 @@ public class ConfirmPayment extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jSlider1 = new javax.swing.JSlider();
         jLabel4 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
@@ -56,6 +57,12 @@ public class ConfirmPayment extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         bookingsTable = new javax.swing.JTable();
         backButton = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        bookingId = new javax.swing.JLabel();
+        eventTitle = new javax.swing.JLabel();
+        total = new javax.swing.JLabel();
+        jToggleButton1 = new javax.swing.JToggleButton();
+        confirm = new javax.swing.JComboBox();
         jMenuBar1 = new javax.swing.JMenuBar();
         file = new javax.swing.JMenu();
         Exit = new javax.swing.JMenuItem();
@@ -118,6 +125,11 @@ public class ConfirmPayment extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
+        bookingsTable.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                bookingsTableMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(bookingsTable);
 
         backButton.setText("Back");
@@ -126,6 +138,26 @@ public class ConfirmPayment extends javax.swing.JFrame {
                 backButtonActionPerformed(evt);
             }
         });
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel5.setText("Choose a booking to confirm on receipt of payment ");
+
+        bookingId.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+
+        eventTitle.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+
+        total.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        total.setText("   ");
+
+        jToggleButton1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jToggleButton1.setText("Save");
+        jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton1ActionPerformed(evt);
+            }
+        });
+
+        confirm.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Not confirmed", "Confirmed" }));
 
         jMenuBar1.setBackground(new java.awt.Color(153, 153, 153));
         jMenuBar1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
@@ -164,32 +196,57 @@ public class ConfirmPayment extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(backButton))
-                    .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(bookingId, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(eventTitle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(18, 18, 18)
+                        .addComponent(total)
+                        .addGap(18, 18, 18)
+                        .addComponent(confirm, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jToggleButton1)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel4)
                 .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel5)
+                .addGap(188, 188, 188))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(158, 158, 158)
-                        .addComponent(jLabel4))
+                        .addComponent(jLabel4)
+                        .addGap(311, 311, 311))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(18, 18, 18)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(18, 18, 18)
+                        .addComponent(jLabel5)
+                        .addGap(16, 16, 16)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(bookingId, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(total)
+                                .addComponent(jToggleButton1)
+                                .addComponent(confirm, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(eventTitle, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                        .addGap(18, 18, 18)))
                 .addComponent(backButton)
-                .addContainerGap(98, Short.MAX_VALUE))
+                .addContainerGap(92, Short.MAX_VALUE))
         );
 
         setSize(new java.awt.Dimension(950, 807));
@@ -221,6 +278,44 @@ public class ConfirmPayment extends javax.swing.JFrame {
         }
         
     }//GEN-LAST:event_backButtonActionPerformed
+
+    private void bookingsTableMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bookingsTableMouseClicked
+        // TODO add your handling code here:
+        DefaultTableModel model =(DefaultTableModel) bookingsTable.getModel();
+        int selectedRowIndex = bookingsTable.getSelectedRow();
+        
+        bookingId.setText(model.getValueAt(selectedRowIndex, 7).toString());
+        eventTitle.setText(model.getValueAt(selectedRowIndex, 1).toString());
+        if(!total.getText().equals("")){
+           total.setText("£" + model.getValueAt(selectedRowIndex, 5).toString()); 
+        }
+        if(model.getValueAt(selectedRowIndex, 8).toString().equals("Not confirmed") || model.getValueAt(selectedRowIndex, 8).toString().equals("") || model.getValueAt(selectedRowIndex, 8).toString().equals(null)){
+            confirm.setVisible(true);
+        } else{
+            confirm.setVisible(false);
+        }
+        
+    }//GEN-LAST:event_bookingsTableMouseClicked
+
+    private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
+        // TODO add your handling code here:
+        String confirmPaymentSQL = "UPDATE `bookings` SET `confirmed` = ? WHERE `bookings`.`id` = ?;";
+        try{
+            confirmPayment = connection.prepareStatement(confirmPaymentSQL);
+            confirmPayment.setString(1, confirm.getSelectedItem().toString());
+            confirmPayment.setString(2, bookingId.getText());
+            int updated = confirmPayment.executeUpdate();
+            
+            if(updated > 0){
+               JOptionPane.showMessageDialog(null, "Payment successfully updated");
+               setVisible(false);
+               ConfirmPayment confPayment = new ConfirmPayment("admin");
+               confPayment.setVisible(true);
+            }
+        } catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }//GEN-LAST:event_jToggleButton1ActionPerformed
 
     public ArrayList<Booking>bookingsList() {
         ArrayList<Booking>bookingsList = new ArrayList<>();
@@ -319,16 +414,23 @@ public class ConfirmPayment extends javax.swing.JFrame {
     private javax.swing.JMenuItem Exit;
     private javax.swing.JMenuItem about_us;
     private javax.swing.JButton backButton;
+    private javax.swing.JLabel bookingId;
     private javax.swing.JTable bookingsTable;
+    private javax.swing.JComboBox confirm;
+    private javax.swing.JLabel eventTitle;
     private javax.swing.JMenu file;
     private javax.swing.JMenu help;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JSlider jSlider1;
+    private javax.swing.JToggleButton jToggleButton1;
     private javax.swing.JMenuItem logout;
+    private javax.swing.JLabel total;
     // End of variables declaration//GEN-END:variables
 }
